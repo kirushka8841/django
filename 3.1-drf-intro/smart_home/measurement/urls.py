44d1e92f6ebad42a757cdf05c539af6f14a1eb82
Kirushka88 <kirushka8841@yandex.ru>
2023-08-12 20:04:02 +0600

    5th

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from measurement.views import MeasurementView, SensorView
 
 urlpatterns = [
-    # TODO: зарегистрируйте необходимые маршруты
+    path('measurement/', MeasurementView.as_view()),
+    path('sensors/<int:pk>/', SensorView.as_view()),
+    path('sensors/', SensorView.as_view())
 ]
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,39 @@
-# TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
-# TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.response import Response
+from .models import Measurement, Sensor
+from .serializers import SensorSerializer, SensorDetailSerializer, MeasurementDetailSerializer
+
+
+class SensorView(ListAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = SensorDetailSerializer
+
+    def post(self, request):
+        Sensor.objects.create(
+            name=request.POST.get('name'),
+            descripcion=request.POST.get('description')
+        )
+        return Response({'status':'Ok'})
+    
+
+class SensorView(RetrieveAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = SensorSerializer
+
+    def patch(self, request, pk): 
+        sensor = Sensor.objects.get(id=pk)
+        sensor.description = request.data['description']
+        sensor.save()
+        return Response({'Status':'Ok'})
+    
+
+class MeasurementView(ListAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementDetailSerializer
+
+    def post(self, request):
+        Measurement.objects.create(
+            sensor_id=request.POST.get('sensor'),
+            temperature=request.POST.get('temperature')
+        )
+        return Response({'status':'Ok'})
